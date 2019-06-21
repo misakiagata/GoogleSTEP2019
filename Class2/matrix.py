4b757f8471abf7e12d883eba632dd14a4e9e6cbf
縣美早 <misakiagata@kenbihayanoMacBook-Pro.local>
2019-06-21 20:00:00 +0900

    Added some files

---
+++ Class2/matrix.py
@@ -0,0 +1,44 @@
+import numpy, sys, time
+
+if (len(sys.argv) != 2):
+    print("usage: python %s N" % sys.argv[0])
+    quit()
+
+n = int(sys.argv[1])
+a = numpy.zeros((n, n)) # Matrix A
+b = numpy.zeros((n, n)) # Matrix B
+c = numpy.zeros((n, n)) # Matrix C
+
+for i in range(n):
+    for j in range(n):
+        a[i, j] = i * n + j
+        b[i, j] = j * n + i
+        c[i, j] = 0
+
+begin = time.time()
+
+for i in range(n): #列
+    for j in range(n): #行
+        for t in range(n): #行の中
+            c[i,j] += a[i,t]*b[t,j] 
+
+
+result = 0
+for i in range(n):
+    for j in range(n):
+        result += c[i,j]
+
+print(result)
+
+end = time.time()
+print("time: %.6f sec" % (end - begin))
+
+# Print C for debugging. Comment out the print before measuring the execution time.
+total = 0
+for i in range(n):
+    for j in range(n):
+        # print c[i, j]
+        total += c[i, j]
+# Print out the sum of all values in C.
+# This should be 450 for N=3, 3680 for N=4, and 18250 for N=5.
+print("sum: %.6f" % total)
